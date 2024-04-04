a32f9479ea0ef1188e806e4a6c54a78ce164dba6
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-04-04 02:48:30 +0000

    Task complete

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,36 +1,26 @@
 #!/usr/bin/env python3
 
-'''FIFO caching
+'''Basic dictionary
 '''
 
 
-from collections import OrderedDict
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    '''A `FIFOCache` cache which inherits from the
-       `BaseCaching` and is a caching system.
+class BasicCache(BaseCaching):
+    '''A `BasicCache` class which inherits from `BaseCaching`
+       and is a caching system
     '''
 
-    def __init__(self):
-        super().__init__()
-        self.cache_data = OrderedDict()
-
     def put(self, key, item):
-        '''assign the `item` value for the key `key` to the `self.cache_data` dictionary
+        '''assign the
+           `item` value for the key `key` to the `self.cache_data` dictionary
         '''
-
-        if key is None or item is None:
-            return
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
-
-        self.cache_data[key] = item
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
-        '''return value in self.cache_data linked to the key
+        '''return the `self.cache_data` value linked to `key`
         '''
-        return self.cache_data.get(key, None)i
+
+        return self.cache_data.get(key, None)
